e6ca20f0550b6968e4c896abf4e889bc0968c8d5
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-17 19:41:55 -0300

    test: adds task creation functional test

--- test/functional/tasks.test.ts
+++ test/functional/tasks.test.ts
@@ -1,16 +1,52 @@
 describe('Tasks functional tests', () => {
-  it('should return a task', async () => {
-    const { body, status } = await global.testRequest.get('/tasks');
-    expect(status).toEqual(200);
-    expect(body).toEqual([
-      {
-        id: '12345',
-        task: 'study',
-      },
-      {
-        id: '678910',
-        task: 'read',
-      },
-    ]);
+  describe('When fetching tasks', () => {
+    it('should return a task', async () => {
+      const { body, status } = await global.testRequest.get('/tasks');
+      expect(status).toEqual(200);
+      expect(body).toEqual([
+        {
+          id: '12345',
+          task: 'study',
+        },
+        {
+          id: '678910',
+          task: 'read',
+        },
+      ]);
+    });
+  });
+
+  describe('When creating a task', () => {
+    it('should create a task with success', async () => {
+      const newTask = {
+        id: '12345678910',
+        order: 20,
+        title: 'study',
+        type: 'binary',
+        status: 'todo',
+        urgent: false,
+        important: false,
+        description: null,
+        registerDate: '2022/10/17',
+        conclusionDate: null,
+      };
+
+      const response = await global.testRequest.post('/tasks').send(newTask);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(newTask);
+    });
   });
 });
+
+// export interface ITask {
+//   id: string;
+//   order: number;
+//   title: string;
+//   type: 'binary' | 'timer' | 'pomodoro';
+//   status: 'onCourse' | 'done' | 'todo';
+//   urgent: boolean;
+//   important: boolean;
+//   description?: string;
+//   registerDate: string;
+//   conclusionDate?: string;
+// }
