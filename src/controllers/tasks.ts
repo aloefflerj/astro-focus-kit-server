2d79f2c95dfd5949e01daadee4215bd1486cd09b
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-18 21:45:58 -0300

    :sparkles: feat: adds invalid field validation on task creation

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post } from '@overnightjs/core';
 import { Task } from '@src/models/task';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 @Controller('tasks')
 export class TasksController {
@@ -36,8 +37,16 @@ export class TasksController {
 
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const task = new Task(req.body);
-    const result = await task.save();
-    res.status(201).send(result);
+    try {
+      const task = new Task(req.body);
+      const result = await task.save();
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
