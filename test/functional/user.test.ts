614f0fc1977a804e28dcc2c1d19dce04d42bce0d
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-19 20:09:33 -0300

    :white_check_mark: test: adds user functional test

---
+++ test/functional/user.test.ts
@@ -0,0 +1,15 @@
+describe('Users functional tests', () => {
+  describe('When creating a new user', () => {
+    it('should successfully creat a new user', async () => {
+      const newUser = {
+        name: 'dovahkiin',
+        email: 'dovahkiin@skyrim.com',
+        password: '12345',
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+  });
+});
