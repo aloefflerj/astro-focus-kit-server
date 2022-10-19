4e31f479b265de5dd87b56d787c67c56c25320b1
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-18 23:44:08 -0300

    :sparkles: feat: adds cors to allow requests temporarily from anywhere

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
+import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import { TasksController } from './controllers/tasks';
+import cors from 'cors';
 import * as database from '@src/database';
-import './util/module-alias';
 
 export class SetupServer extends Server {
   constructor(private port = 3000 || process.env.port) {
@@ -18,6 +19,11 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupControllers(): void {
