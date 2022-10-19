1e79f5bf15581fa7b6e2fdc3d66828c56f302cf7
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-18 22:20:04 -0300

    :test_tube: test: adds fixtures to task response testing

--- test/functional/tasks.test.ts
+++ test/functional/tasks.test.ts
@@ -1,19 +1,10 @@
 import { Task } from '@src/models/task';
+import taskResponseFixture from '@test/fixtures/taskResponseFixture.json';
 
 describe('Tasks functional tests', () => {
   beforeAll(async () => await Task.deleteMany({}));
   beforeEach(async () => {
-    const defaultTask = {
-      order: 2,
-      title: 'piano',
-      type: 'binary',
-      status: 'todo',
-      urgent: false,
-      important: false,
-      description: null,
-      registerDate: '2022/10/17',
-      conclusionDate: null,
-    };
+    const defaultTask = taskResponseFixture;
     const task = new Task(defaultTask);
     await task.save();
   });
@@ -21,35 +12,13 @@ describe('Tasks functional tests', () => {
     it('should return a task', async () => {
       const { body, status } = await global.testRequest.get('/tasks');
       expect(status).toEqual(200);
-      expect(body).toEqual([
-        expect.objectContaining({
-          order: 2,
-          title: 'study',
-          type: 'binary',
-          status: 'todo',
-          urgent: false,
-          important: false,
-          description: null,
-          registerDate: '2022-10-17T03:00:00.000Z',
-          conclusionDate: null,
-        }),
-      ]);
+      expect(body).toEqual([expect.objectContaining(taskResponseFixture)]);
     });
   });
 
   describe('When creating a task', () => {
     it('should create a task with success', async () => {
-      const newTask = {
-        order: 3,
-        title: 'study',
-        type: 'binary',
-        status: 'todo',
-        urgent: false,
-        important: false,
-        description: null,
-        registerDate: '2022-10-19T03:00:00.000Z',
-        conclusionDate: null,
-      };
+      const newTask = taskResponseFixture;
 
       const response = await global.testRequest.post('/tasks').send(newTask);
       expect(response.status).toBe(201);
@@ -57,17 +26,7 @@ describe('Tasks functional tests', () => {
     });
 
     it('should return 422 when there is a validation error', async () => {
-      const newTask = {
-        order: 3,
-        title: 'study',
-        type: 'binary',
-        status: 'todo',
-        urgent: 'THIS IS NOT A VALID VALUE',
-        important: false,
-        description: null,
-        registerDate: '2022-10-19T03:00:00.000Z',
-        conclusionDate: null,
-      };
+      const newTask = taskResponseFixture;
       const response = await global.testRequest.post('/tasks').send(newTask);
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
