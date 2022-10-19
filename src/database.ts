052cac52bb4e5d056b77e3c61ef5aa108f4a51e9
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-18 21:11:06 -0300

    :bug: fix: fix connection to mongo by environment config

--- src/database.ts
+++ src/database.ts
@@ -2,9 +2,17 @@ import config, { IConfig } from 'config';
 import { connect as mongooseConnect, connection } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
+const user = dbConfig.get('user');
+const pass = dbConfig.get('pass');
+const host = dbConfig.get('host');
+const port = dbConfig.get('port');
+const db = dbConfig.get('db');
+const authSource = dbConfig.get('authSource');
 
 export const connect = async (): Promise<void> => {
-  await mongooseConnect(dbConfig.get('mongoUrl'));
+  await mongooseConnect(
+    `mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=${authSource}`
+  );
 };
 
 export const close = (): Promise<void> => connection.close();
