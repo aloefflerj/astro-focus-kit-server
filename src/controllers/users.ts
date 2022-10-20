fe953a44c7d3a196d21cee7e318f87a271191f2e
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-19 21:34:56 -0300

    :green_check_mark: test: passes invalid new user test

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,13 +1,20 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 @Controller('users')
 export class UsersController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const user = new User(req.body);
-    const newUser = await user.save();
-    res.status(201).send(newUser);
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
+      res.status(201).send(newUser);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(400).send({ error: error.message });
+      }
+    }
   }
 }
