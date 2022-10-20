9b52e7fe2be77045157b2948852523c1fe3ba864
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-20 18:58:55 -0300

    :white_check_mark: test: adds token verification error validation test

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -1,5 +1,6 @@
 import AuthService from '@src/services/auth';
 import { authMiddleware } from '@src/middlewares/auth';
+import { StatusCodes } from 'http-status-codes';
 
 describe('Auth Middleware', () => {
   it('should verify a JWT and call the next middleware', () => {
@@ -13,5 +14,28 @@ describe('Auth Middleware', () => {
     const nextFake = jest.fn();
     authMiddleware(reqFake, resFake, nextFake);
     expect(nextFake).toHaveBeenCalled();
+  });
+
+  it('should return UNAUTHORIZED if there is a problem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid token',
+      },
+    };
+
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+
+    expect(resFake.status).toHaveBeenCalledWith(StatusCodes.UNAUTHORIZED);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: StatusCodes.UNAUTHORIZED,
+      error: 'jwt malformed',
+    });
   });
 });
