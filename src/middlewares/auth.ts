2a4eeddf0eeb62eeb40fffbb2c93e40bf366b402
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-20 19:12:55 -0300

    :sparkles: feat: handles unauthorized jwt for invalid token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,14 +1,23 @@
 import AuthService from '@src/services/auth';
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 export function authMiddleware(
   req: Partial<Request>,
-  _: Partial<Response>,
+  res: Partial<Response>,
   next: NextFunction
 ): void {
   const token = req.headers?.['x-access-token'];
-  // TODO: type token correctly
-  const decoded = AuthService.decodeToken(token as string);
-  req.decoded = decoded;
+  try {
+    // TODO: type token correctly
+    const decoded = AuthService.decodeToken(token as string);
+    req.decoded = decoded;
+  } catch (error) {
+    if (!(error instanceof Error)) return;
+
+    res
+      .status?.(StatusCodes.UNAUTHORIZED)
+      .send({ code: StatusCodes.UNAUTHORIZED, error: error.message });
+  }
   next();
 }
