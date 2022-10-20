6bff3b70ef8d3f093d0a2f21e078b5752a715f93
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-20 01:04:08 -0300

    :sparkles: feat: generates jwt on user authentication

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { BaseController } from '.';
@@ -22,5 +23,22 @@ export class UsersController extends BaseController {
         error: 'User validation failed: name: Path `name` is required.',
       });
     }
+  }
+
+  @Post('auth')
+  public async auth(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      // todo => handle user not found
+      return;
+    }
+
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      // todo => handle wrong password
+      return;
+    }
+    const token = AuthService.generateToken(user.toJSON());
+    res.status(200).send({ token: token });
   }
 }
