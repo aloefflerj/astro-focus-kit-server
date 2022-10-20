9c67d6179c51d2e2c144d66581148fae0534cba7
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-10-20 19:15:18 -0300

    :heavy_check_mark: adds and passes unauthorized auth middleware on empty jwt header test

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -38,4 +38,25 @@ describe('Auth Middleware', () => {
       error: 'jwt malformed',
     });
   });
+
+  it('should return UNAUTHORIZED middleware if there is no token', () => {
+    const reqFake = {
+      headers: {},
+    };
+
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+
+    expect(resFake.status).toHaveBeenCalledWith(StatusCodes.UNAUTHORIZED);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: StatusCodes.UNAUTHORIZED,
+      error: 'jwt must be provided',
+    });
+  });
 });
