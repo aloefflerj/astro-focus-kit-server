6722ab19964d7de38396c8f224d868b845a461b8
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-14 20:50:41 -0300

    :sparkles: feat: adds required auth ping

--- src/controllers/ping.ts
+++ src/controllers/ping.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, Middleware } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { BaseController } from '.';
@@ -10,9 +11,26 @@ export class PingController extends BaseController {
   }
 
   @Get('')
-  public async ping(req: Request, res: Response): Promise<void> {
+  public async ping(_: Request, res: Response): Promise<void> {
     try {
-      //   const user = req.decoded?.id;
+      res.status(StatusCodes.OK).send();
+      return;
+    } catch (error) {
+      if (error instanceof Error) {
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .send({ code: StatusCodes.BAD_REQUEST, error: error.message });
+        return;
+      }
+
+      this.internalServerError(res);
+    }
+  }
+
+  @Get('auth')
+  @Middleware(authMiddleware)
+  public async pingAuth(_: Request, res: Response): Promise<void> {
+    try {
       res.status(StatusCodes.OK).send();
       return;
     } catch (error) {
