d302b007dd1c1ce2375bc9bb19f9243e0528aed9
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-14 21:02:45 -0300

    :heavy_check_mark: test: passes successful required auth ping

--- test/functional/ping.test.ts
+++ test/functional/ping.test.ts
@@ -1,3 +1,5 @@
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { StatusCodes } from 'http-status-codes';
 
 describe('Ping functional tests', () => {
@@ -12,6 +14,24 @@ describe('Ping functional tests', () => {
     it("should return 401 status if a given user isn't logged in", async () => {
       const { status } = await global.testRequest.get('/ping/auth');
       expect(status).toBe(StatusCodes.UNAUTHORIZED);
+    });
+
+    it('should return 200 status if a given user is logged in', async () => {
+      await User.deleteMany({});
+
+      const user = await new User({
+        name: 'dovahkiin',
+        email: 'dovahkiin@skyrim.com',
+        password: '12345',
+      }).save();
+
+      const token = AuthService.generateToken(user.toJSON());
+
+      const { status } = await global.testRequest
+        .get('/ping/auth')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(StatusCodes.OK);
     });
   });
 });
