56ca223562def763545bb70d42705e5242a3b62a
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-16 00:54:45 -0300

    :white_check_mark: test: adds default websites response if the user has no config test

---
+++ src/clients/defaultValues/defaultBlockedWebsites.ts
@@ -0,0 +1,46 @@
+export const defaultBlockedWebsites = [
+  {
+    id: '&8CcqqU2c5Gnhg',
+    url: 'yotube.com',
+  },
+  {
+    id: 'Yd28ENg&!X!Aiz',
+    url: 'instagram.com',
+  },
+  {
+    id: 'p3Dh4Qf$SM8$93',
+    url: 'facebook.com',
+  },
+  {
+    id: '2rK23*%&!2EApI',
+    url: 'twitter.com',
+  },
+  {
+    id: 'qS6!H42RMNOw%E',
+    url: 'linkedin.com',
+  },
+  {
+    id: '73C@u4nnJ!KNbX',
+    url: 'netflix.com',
+  },
+  {
+    id: '0l80H0Wr5&I4GT',
+    url: 'reddit.com',
+  },
+  {
+    id: 'Cx0%r67dTKd*no',
+    url: 'twitch.com',
+  },
+  {
+    id: 'Wh646#u71^uaNy',
+    url: 'pinterest.com',
+  },
+  {
+    id: '7dS44^bhQXqakP',
+    url: 'g1.com',
+  },
+  {
+    id: '0b03T*pYQDkJsG',
+    url: 'globo.com',
+  },
+];
---
+++ test/functional/blocks.test.ts
@@ -0,0 +1,33 @@
+import { defaultBlockedWebsites } from '@src/clients/defaultValues/defaultBlockedWebsites';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
+import { StatusCodes } from 'http-status-codes';
+
+describe('Blocks functional tests', () => {
+  const defaultUser = {
+    name: 'dovahkiin',
+    email: 'dovahkiin@skyrim.com',
+    password: '12345',
+  };
+
+  let token: string;
+
+  beforeEach(async () => {
+    await Block.deleteMany({});
+    await User.deleteMany({});
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
+  });
+
+  describe('When fetching blocks from a given user', () => {
+    it('should return default blocks if the user has no config', async () => {
+      const { status, body } = await global.testRequest
+        .get('/blocks')
+        .set({ 'x-access-token': token });
+
+      expect(status).toEqual(StatusCodes.OK);
+
+      expect(body).toEqual(defaultBlockedWebsites);
+    });
+  });
+});
