1ee966e2fd9e89da381a87f5706a53dce438e1c7
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-20 20:01:38 -0300

    :bug: fix: fixes dastabase connection url

--- src/database.ts
+++ src/database.ts
@@ -2,18 +2,10 @@ import config, { IConfig } from 'config';
 import { connect as mongooseConnect, connection } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
-const prefix = dbConfig.get('prefix');
-const user = dbConfig.get('user');
-const pass = dbConfig.get('pass');
-const host = dbConfig.get('host');
-const port = dbConfig.get('port');
-const db = dbConfig.get('db');
-const authSource = dbConfig.get('authSource');
+const mongoConnection = dbConfig.get<string>('connection');
 
 export const connect = async (): Promise<void> => {
-  await mongooseConnect(
-    `mongodb://${user}:${pass}@${host}:${port}/${db}?authSource=${authSource}`
-  );
+  await mongooseConnect(mongoConnection);
 };
 
 export const close = (): Promise<void> => connection.close();
