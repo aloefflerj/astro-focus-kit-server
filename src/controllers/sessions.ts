3547f0edb0eed3bac2f5d4b63a806dfa2fa197fe
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-22 23:51:15 -0300

    :sparkles: feat: finds and update session status by user

--- src/controllers/sessions.ts
+++ src/controllers/sessions.ts
@@ -33,11 +33,40 @@ export class SessionsController extends BaseController {
   }
 
   @Patch(':id')
-  public async update(req: Request, res: Response): Promise<void> {
+  public async updateStatus(req: Request, res: Response): Promise<void> {
     try {
       const { id } = <{ id: string }>req.params;
       const { status } = req.body;
       const response = await Session.findByIdAndUpdate(id, { status });
+
+      if (!response) {
+        res
+          .status(StatusCodes.NOT_FOUND)
+          .send({ code: StatusCodes.NOT_FOUND, error: 'Session not found' });
+
+        return;
+      }
+
+      res.status(StatusCodes.NO_CONTENT).send();
+      return;
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        this.sendCreateUpdateErrorResponse(res, error);
+        return;
+      }
+    }
+    this.internalServerError(res);
+  }
+
+  @Patch('')
+  public async updateStatusByUser(req: Request, res: Response): Promise<void> {
+    try {
+      const { status } = req.body;
+
+      const response = await Session.findOneAndUpdate(
+        { user: req.decoded?.id },
+        { status }
+      );
 
       if (!response) {
         res
