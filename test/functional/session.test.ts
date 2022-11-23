9fba60bf067dc9fa3c3dc3ddcd9289955f03de3f
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-22 21:41:14 -0300

    :white_check_mark: test: adds default session status for user test

---
+++ test/functional/session.test.ts
@@ -0,0 +1,42 @@
+import { User } from '@src/models/user';
+import { StatusCodes } from 'http-status-codes';
+import moment from 'moment-timezone';
+
+describe('Sessions functional tests', () => {
+  const defaultUser = {
+    name: 'dovahkiin',
+    email: 'dovahkiin@skyrim.com',
+    password: '12345',
+  };
+
+  let token: string;
+
+  beforeEach(async () => {
+    await global.testRequest.post('/users').send(defaultUser);
+
+    const { body: userBody } = await global.testRequest
+      .post('/users/auth')
+      .send({ email: defaultUser.email, password: defaultUser.password });
+
+    token = userBody.token;
+  });
+
+  afterAll(async () => {
+    await User.deleteMany({});
+  });
+
+  describe('When creating a user', () => {
+    it('should create a focusing session by default', async () => {
+      const newSession = {
+        status: 'focusing',
+      };
+
+      const { status, body } = await global.testRequest
+        .get('/session')
+        .set({ 'x-access-token': token });
+
+      expect(status).toBe(StatusCodes.CREATED);
+      expect(body).toEqual(expect.objectContaining(newSession));
+    });
+  });
+});
