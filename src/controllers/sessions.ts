b1750c758317ab8f71e0e2e800ba294fb67f1fd6
Anderson Job Loeffler <aloeffler.j@gmail.com>
2022-11-22 23:36:18 -0300

    :sparkles: feat: adds change to session status implementation

--- src/controllers/sessions.ts
+++ src/controllers/sessions.ts
@@ -1,4 +1,4 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get, Patch } from '@overnightjs/core';
 import { authMiddleware } from '@src/middlewares/auth';
 import { restrictedOrigin } from '@src/middlewares/restrictedOrigin';
 import { Session } from '@src/models/session';
@@ -26,6 +26,32 @@ export class SessionsController extends BaseController {
         res
           .status(StatusCodes.BAD_REQUEST)
           .send({ code: StatusCodes.BAD_REQUEST, error: error.message });
+        return;
+      }
+    }
+    this.internalServerError(res);
+  }
+
+  @Patch(':id')
+  public async update(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = <{ id: string }>req.params;
+      const { status } = req.body;
+      const response = await Session.findByIdAndUpdate(id, { status });
+
+      if (!response) {
+        res
+          .status(StatusCodes.NOT_FOUND)
+          .send({ code: StatusCodes.NOT_FOUND, error: 'Session not found' });
+
+        return;
+      }
+
+      res.status(StatusCodes.NO_CONTENT).send();
+      return;
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        this.sendCreateUpdateErrorResponse(res, error);
         return;
       }
     }
